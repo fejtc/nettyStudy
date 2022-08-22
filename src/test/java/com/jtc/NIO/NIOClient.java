dce01ed291f78355c31c3483ee3e5256b56d0ed4
fejtc <2504553272@qq.com>
2022-08-22 14:52:01 +0800

    fe add NIO Blocking and NoBlocking

--- src/test/java/com/jtc/NIO/NIOClient.java
+++ src/test/java/com/jtc/NIO/NIOClient.java
@@ -1,19 +1,13 @@
-package com.jtc.feNIO;
+package com.jtc.NIO;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
 import java.nio.channels.SocketChannel;
 
-public class Client {
+public class NIOClient {
     public static void main(String[] args) throws IOException {
-        //创建服务器
         SocketChannel sc = SocketChannel.open();
         sc.connect(new InetSocketAddress("localhost", 8080));
-        SocketAddress address = sc.getLocalAddress();
         System.out.println("waiting...");
-        //        sc.close();
-        //System.out.println();
-
     }
 }
--- src/test/java/com/jtc/nettytest/TestByteBufferReadWrite.java
+++ src/test/java/com/jtc/nettytest/TestByteBufferReadWrite.java
@@ -1,6 +1,7 @@
 package com.jtc.nettytest;
 
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 
 import static com.jtc.nettytest.ByteBufferUtil.debugAll;
 
@@ -8,13 +9,19 @@ public class TestByteBufferReadWrite {
     public static void main(String[] args) {
         ByteBuffer buffer = ByteBuffer.allocate(10);
         buffer.put((byte) 0x61);
-        debugAll(buffer);
+        //debugAll(buffer);
 
         buffer.put(new byte[]{0x65,0x66,0x64});
-        debugAll(buffer);
+        //debugAll(buffer);
+
 
         //堆内存   与   直接内存
         System.out.println(ByteBuffer.allocate(15).getClass());
         System.out.println(ByteBuffer.allocateDirect(15).getClass());
+
+
+
+        System.out.println(Charset.defaultCharset().decode(buffer));
+
     }
 }
